8fda750fd3a88cb95f0396174f6bd3aa94af28a2
lucymell <99334158+lucymell@users.noreply.github.com>
2022-09-19 14:45:24 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs puppet-lint, version 2.1.1
+# install flask 2.1.0 from pip3
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'flask':
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
 }
